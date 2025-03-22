d6dba62532aea30d4b5c79a791903391929963f3
DD199427 <56765674+DD199427@users.noreply.github.com>
2025-03-22 22:05:37 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Booking from "./components/Booking";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
